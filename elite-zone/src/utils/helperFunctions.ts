16d3a309dbccd45cb4ef8d81dca1e4d88f0f39d0
Phan Anh Tuan <tuanp6905@gmail.com>
2023-05-25 21:53:20 +0700

    setup axios (#4)

---
+++ elite-zone/src/utils/helperFunctions.ts
@@ -0,0 +1,14 @@
+type JwtPayloadType = any
+export const decodeJwt = (token: string): JwtPayloadType => {
+  if (!token) return null
+  const base64Url = token.split('.')[1]
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+      .join('')
+  )
+
+  return JSON.parse(jsonPayload)
+}
