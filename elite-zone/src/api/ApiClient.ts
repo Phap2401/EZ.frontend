efc187afb4b7dd9bc1973d4401bb360c809101e8
Phan Anh Tuan <tuanp6905@gmail.com>
2023-10-08 14:19:29 +0700

    fix eslint error

--- elite-zone/src/api/ApiClient.ts
+++ elite-zone/src/api/ApiClient.ts
@@ -1,12 +1,12 @@
-import axios, { AxiosResponse, AxiosRequestConfig } from 'axios'
+import axios, { AxiosResponse, AxiosRequestConfig } from 'axios';
 
-import { stringify } from 'qs'
-import moment from 'moment'
-import { decodeJwt } from '~/utils/helperFunctions'
+import { stringify } from 'qs';
+import moment from 'moment';
+import { decodeJwt } from '~/utils/helperFunctions';
 
-const domainUrl = `${import.meta.env.VITE_REACT_APP_API_HOST}/api`
+const domainUrl = `${import.meta.env.VITE_REACT_APP_API_HOST}/api`;
 
-export type QueryObject = { [key: string]: string | number | boolean | null }
+export type QueryObject = { [key: string]: string | number | boolean | null };
 
 export default class ApiClient {
   /**
@@ -19,17 +19,17 @@ export default class ApiClient {
     params: object,
     query?: undefined | { [key: string]: string | boolean | null } | string
   ): Promise<AxiosResponse> {
-    let requestUrl = query ? `${url}?${stringify(query)}` : url
+    let requestUrl = query ? `${url}?${stringify(query)}` : url;
     if (typeof query === 'string') {
-      requestUrl = `${url}${query}`
+      requestUrl = `${url}${query}`;
     }
-    console.log('domainUrl', domainUrl)
+    console.log('domainUrl', domainUrl);
     const response = await axios.get(domainUrl + requestUrl, {
       params,
       headers: await this.getHeaders(),
-      data: {}
-    })
-    return response
+      data: {},
+    });
+    return response;
   }
 
   static async getNoHeader(
@@ -37,95 +37,102 @@ export default class ApiClient {
     params: object,
     query?: undefined | { [key: string]: string | boolean | null } | string
   ): Promise<AxiosResponse> {
-    let requestUrl = query ? `${url}?${stringify(query)}` : url
+    let requestUrl = query ? `${url}?${stringify(query)}` : url;
     if (typeof query === 'string') {
-      requestUrl = `${url}${query}`
+      requestUrl = `${url}${query}`;
     }
     const response = await axios.get(domainUrl + requestUrl, {
       params,
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      data: {}
-    })
-    return response
+      data: {},
+    });
+    return response;
   }
 
   static async getHeaders(contentType = 'application/x-www-form-urlencoded') {
     return {
       'Content-Type': contentType,
-      authorization: await this.getToken()
-    }
+      authorization: await this.getToken(),
+    };
   }
 
   private static async getToken() {
-    const timeClear = 5 // as minutes
-    const timeNow = moment()
+    const timeClear = 5; // as minutes
+    const timeNow = moment();
     //get Token from user info
     const accessToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
     if (!accessToken) {
       // await logOut();// function logout user
-      return ''
+      return '';
     }
     //access token get from API login
-    const expiredTokenAPI = decodeJwt(accessToken)?.exp
-    const isCountExpiredAPITime = moment.duration(moment.unix(expiredTokenAPI ?? 0).diff(timeNow)).asMinutes()
+    const expiredTokenAPI = decodeJwt(accessToken)?.exp;
+    const isCountExpiredAPITime = moment
+      .duration(moment.unix(expiredTokenAPI ?? 0).diff(timeNow))
+      .asMinutes();
     if (isCountExpiredAPITime > timeClear) {
-      return `Bearer ${accessToken}`
+      return `Bearer ${accessToken}`;
     } else {
       // await logOut();// function logout user
-      return ''
+      return '';
     }
   }
 
   private static convertToPostData(obj: any, form: any, namespace: any) {
-    const fd = form || new URLSearchParams()
-    let formKey
+    const fd = form || new URLSearchParams();
+    let formKey;
 
     for (const property in obj) {
       if (obj.hasOwnProperty(property)) {
         if (namespace) {
           if (!isNaN(Number(property))) {
-            formKey = `${namespace}[${property}]`
+            formKey = `${namespace}[${property}]`;
           } else {
-            formKey = `${namespace}.${property}`
+            formKey = `${namespace}.${property}`;
           }
         } else {
-          formKey = property
+          formKey = property;
         }
 
         if (obj[property] instanceof Date) {
-          fd.append(formKey, obj[property].toISOString())
+          fd.append(formKey, obj[property].toISOString());
         } else if (obj[property] instanceof Array<File>) {
           for (const i of Object.keys(obj[property])) {
-            fd.append(formKey, obj[property][i])
+            fd.append(formKey, obj[property][i]);
           }
         } else if (
           typeof obj[property] === 'object' &&
           !(obj[property] instanceof File) &&
           !(obj[property] instanceof Blob)
         ) {
-          this.convertToPostData(obj[property], fd, formKey)
+          this.convertToPostData(obj[property], fd, formKey);
         } else {
-          fd.append(formKey, obj[property])
+          fd.append(formKey, obj[property]);
         }
       }
     }
 
-    return fd
+    return fd;
   }
 
-  static async post(url: string, query: QueryObject, params: any, appendUrl?: string): Promise<AxiosResponse> {
-    const requestUrl = `${url}?${stringify(query)}${appendUrl || ''}`
+  static async post(
+    url: string,
+    query: QueryObject,
+    params: any,
+    appendUrl?: string
+  ): Promise<AxiosResponse> {
+    const requestUrl = `${url}?${stringify(query)}${appendUrl || ''}`;
 
     const config: AxiosRequestConfig = {
-      headers: await this.getHeaders()
-    }
+      headers: await this.getHeaders(),
+    };
 
-    const param = this.convertToPostData(params, undefined, undefined)
-    const response = await axios.post(domainUrl + requestUrl, param, config)
-    return response
+    const param = this.convertToPostData(params, undefined, undefined);
+    const response = await axios.post(domainUrl + requestUrl, param, config);
+    return response;
   }
 
   static async getJsonData(
@@ -133,16 +140,16 @@ export default class ApiClient {
     params: object,
     query?: undefined | { [key: string]: string | boolean } | string
   ): Promise<AxiosResponse> {
-    let requestUrl = query ? `${url}?${stringify(query)}` : url
+    let requestUrl = query ? `${url}?${stringify(query)}` : url;
     if (typeof query === 'string') {
-      requestUrl = `${url}${query}`
+      requestUrl = `${url}${query}`;
     }
     const response = await axios.get(domainUrl + requestUrl, {
       params,
       headers: await this.getHeaders('application/json'),
-      data: {}
-    })
-    return response
+      data: {},
+    });
+    return response;
   }
 
   static async postJsonData(
@@ -151,15 +158,15 @@ export default class ApiClient {
     params: any,
     extraConfig?: Partial<AxiosRequestConfig>
   ): Promise<AxiosResponse> {
-    const requestUrl = `${url}?${stringify(query)}`
+    const requestUrl = `${url}?${stringify(query)}`;
 
     const config: AxiosRequestConfig = {
       headers: await this.getHeaders('application/json'),
-      ...extraConfig
-    }
+      ...extraConfig,
+    };
 
-    const response = await axios.post(domainUrl + requestUrl, params, config)
-    return response
+    const response = await axios.post(domainUrl + requestUrl, params, config);
+    return response;
   }
 
   static async postJsonDataNoHeader(
@@ -168,50 +175,54 @@ export default class ApiClient {
     params: any,
     extraConfig?: Partial<AxiosRequestConfig>
   ): Promise<AxiosResponse> {
-    const requestUrl = `${url}?${stringify(query)}`
+    const requestUrl = `${url}?${stringify(query)}`;
 
     const config: AxiosRequestConfig = {
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      ...extraConfig
-    }
+      ...extraConfig,
+    };
 
-    const response = await axios.post(domainUrl + requestUrl, params, config)
-    return response
+    const response = await axios.post(domainUrl + requestUrl, params, config);
+    return response;
   }
 
   static async putJsonData(url: string, query: QueryObject, params: any): Promise<AxiosResponse> {
-    const requestUrl = `${url}?${stringify(query)}`
+    const requestUrl = `${url}?${stringify(query)}`;
 
     const config: AxiosRequestConfig = {
-      headers: await this.getHeaders('application/json')
-    }
+      headers: await this.getHeaders('application/json'),
+    };
 
-    const response = await axios.put(domainUrl + requestUrl, params, config)
-    return response
+    const response = await axios.put(domainUrl + requestUrl, params, config);
+    return response;
   }
 
   static async delete(url: string, params: any): Promise<AxiosResponse> {
-    const requestUrl = `${url}?${stringify(params)}`
+    const requestUrl = `${url}?${stringify(params)}`;
     const config: AxiosRequestConfig = {
-      headers: await this.getHeaders('application/json')
-    }
-    const response = await axios.delete(domainUrl + requestUrl, config)
-    return response
+      headers: await this.getHeaders('application/json'),
+    };
+    const response = await axios.delete(domainUrl + requestUrl, config);
+    return response;
   }
 
-  static async postMutipartData(url: string, query: QueryObject, params: any): Promise<AxiosResponse> {
-    const requestUrl = `${url}?${stringify(query)}`
+  static async postMutipartData(
+    url: string,
+    query: QueryObject,
+    params: any
+  ): Promise<AxiosResponse> {
+    const requestUrl = `${url}?${stringify(query)}`;
     const config: AxiosRequestConfig = {
-      headers: await this.getHeaders('multipart/form-data')
-    }
-    const form = new FormData()
-    const param = this.convertToPostData(params, form, undefined)
+      headers: await this.getHeaders('multipart/form-data'),
+    };
+    const form = new FormData();
+    const param = this.convertToPostData(params, form, undefined);
 
-    const response = await axios.post(domainUrl + requestUrl, param, config)
+    const response = await axios.post(domainUrl + requestUrl, param, config);
 
-    return response
+    return response;
   }
 
   static async downloadExcelPost(
@@ -221,23 +232,23 @@ export default class ApiClient {
     fileName = 'excel_table',
     isFinished = true
   ): Promise<AxiosResponse> {
-    let requestUrl = query ? `${url}?${stringify(query)}` : url
+    let requestUrl = query ? `${url}?${stringify(query)}` : url;
     if (typeof query === 'string') {
-      requestUrl = `${url}${query}`
+      requestUrl = `${url}${query}`;
     }
     const config: AxiosRequestConfig = {
       headers: await this.getHeaders('application/json'),
-      responseType: 'blob'
-    }
+      responseType: 'blob',
+    };
 
-    const response = await axios.post(domainUrl + requestUrl, params, config)
+    const response = await axios.post(domainUrl + requestUrl, params, config);
     if (isFinished) {
       const blob = new Blob([response.data], {
-        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-      })
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
       // saveAs(blob, fileName)
     }
-    return response
+    return response;
   }
 
   static async downloadExcelGet(
@@ -246,22 +257,22 @@ export default class ApiClient {
     query?: undefined | { [key: string]: string | boolean } | string,
     fileName?: string
   ) {
-    let requestUrl = query ? `${url}?${stringify(query)}` : url
+    let requestUrl = query ? `${url}?${stringify(query)}` : url;
     if (typeof query === 'string') {
-      requestUrl = `${url}${query}`
+      requestUrl = `${url}${query}`;
     }
     const response = await axios.get(domainUrl + requestUrl, {
       params,
       headers: await this.getHeaders('application/xlsx'),
       responseType: 'blob',
-      data: {}
-    })
+      data: {},
+    });
 
     const blob = new Blob([response.data], {
-      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    })
+      type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+    });
     // saveAs(blob, fileName)
-    return response
+    return response;
   }
 
   static async downloadExcelGetXlsm(
@@ -270,22 +281,22 @@ export default class ApiClient {
     query?: undefined | { [key: string]: string | boolean | number } | string,
     fileName?: string
   ) {
-    let requestUrl = query ? `${url}?${stringify(query)}` : url
+    let requestUrl = query ? `${url}?${stringify(query)}` : url;
     if (typeof query === 'string') {
-      requestUrl = `${url}${query}`
+      requestUrl = `${url}${query}`;
     }
     const response = await axios.get(domainUrl + requestUrl, {
       params,
       headers: await this.getHeaders('application/vnd.ms-excel'),
       responseType: 'blob',
-      data: {}
-    })
+      data: {},
+    });
 
     const blob = new Blob([response.data], {
-      type: 'application/vnd.ms-excel'
-    })
+      type: 'application/vnd.ms-excel',
+    });
     // saveAs(blob, fileName)
-    return response
+    return response;
   }
 
   static async uploadFileExcelPostMutipartData(
@@ -294,13 +305,13 @@ export default class ApiClient {
     params: any,
     option?: any
   ): Promise<AxiosResponse> {
-    const requestUrl = `${url}?${stringify(query)}`
+    const requestUrl = `${url}?${stringify(query)}`;
     const config: AxiosRequestConfig = {
       headers: await this.getHeaders('multipart/form-data'),
-      onUploadProgress: option?.onUploadProgress
-    }
-    const response = await axios.post(domainUrl + requestUrl, params, config)
+      onUploadProgress: option?.onUploadProgress,
+    };
+    const response = await axios.post(domainUrl + requestUrl, params, config);
 
-    return response
+    return response;
   }
 }
